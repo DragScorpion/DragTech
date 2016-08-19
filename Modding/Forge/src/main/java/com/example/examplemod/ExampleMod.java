06039b74cd0144b81d2b7a92a8acc16f9aee0ef6
Scornexon <iiscornii@live.de>
2016-08-19 19:50:06 +0200

    Setup Forge Modding
    Setup für das modden mit forge mit multi modding support.

--- Modding/Forge/src/main/java/com/example/examplemod/ExampleMod.java
+++ Modding/Forge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,10 +1,5 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
-
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
