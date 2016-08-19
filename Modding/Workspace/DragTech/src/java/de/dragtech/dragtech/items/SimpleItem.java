7e41aaee3cbf9d91d95059c569cad35c037c72ab
Scornexon <iiscornii@live.de>
2016-08-19 20:30:42 +0200

    Items, CreativeTab und Language File
    Ich habe die Piston items hinzugefügt.
    Ich habe ein CreativeTab hinzugefügt.
    Ich habe eine Language File erstellt.

---
+++ Modding/Workspace/DragTech/src/java/de/dragtech/dragtech/items/SimpleItem.java
@@ -0,0 +1,15 @@
+package de.dragtech.dragtech.items;
+
+import de.dragtech.dragtech.Main;
+import de.dragtech.dragtech.utilities.MainUtilities;
+import net.minecraft.item.Item;
+
+public class SimpleItem extends Item {
+	
+	public SimpleItem(String unlocalizedName) {
+		this.setUnlocalizedName(unlocalizedName);
+		this.setTextureName(Main.MODID + ":" + unlocalizedName);
+		this.setCreativeTab(MainUtilities.tabDragTech);
+		this.setMaxStackSize(1);
+	}
+}
