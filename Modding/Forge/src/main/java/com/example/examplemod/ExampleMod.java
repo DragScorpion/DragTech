a89d6c7ac75975c2c798365d16672982d90820d6
Scornexon <iiscornii@live.de>
2016-08-19 19:57:16 +0200

    Forge Bug
    Die vorherige Forge installation war fehlerhaft.
    Ich habe die alte umbenannt und eine funktionierende hinzugefügt.

--- Modding/Forge/src/main/java/com/example/examplemod/ExampleMod.java
+++ Modding/Forge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,10 @@
 package com.example.examplemod;
 
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
